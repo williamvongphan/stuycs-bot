bb62d5ce02465606bb7e5e62ffee1e0a9a10120f
ThunderRedStar <thunderredstar@thunderredstar.tech>
2022-09-09 09:16:26 -0400

    test

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const checkForNewEntries = require("../utils/checkForNewEntries");
+const parseEntryInformation = require("../utils/parseEntryInformation");
 
 let validClassNames = [
 	"apcs-mykolyk",
@@ -57,4 +59,50 @@ module.exports = (app) => {
 			}
 		}
 	});
+
+	setInterval(async () => {
+		console.log("Checking for new entries...");
+		// Scrape the feeds for new entries.
+		let targetRepos = [];
+		let newEntries = {};
+		newEntries["apcs-mykolyk"] = await checkForNewEntries("https://www.stuycs.org/apcs-mykolyk/feed.xml", "apcs-mykolyk.xml");
+		newEntries["systems-dw"] = await checkForNewEntries("https://www.stuycs.org/systems-dw/feed.xml", "systems-dw.xml");
+		newEntries["graphics-dw"] = await checkForNewEntries("https://www.stuycs.org/graphics-dw/feed.xml", "graphics-dw.xml");
+		newEntries["softdev-mykolyk"] = await checkForNewEntries("https://www.stuycs.org/softdev-mykolyk/feed.xml", "softdev-mykolyk.xml");
+		newEntries["staging"] = await checkForNewEntries("https://stuycs-bot-test.thundrredstar.repl.co/feed.xml", "staging.xml");
+
+		// Now we'll fetch the data for each class and create an issue for each new entry.
+		const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "data.json"), { encoding: "utf8" }));
+		for (let className in newEntries) {
+			if (newEntries[className].length > 0) {
+				const repos = data[className];
+				for (let entry of newEntries[className]) {
+					const entryInfo = parseEntryInformation(entry);
+					for (let repo of repos) {
+						targetRepos.push({
+							repo: repo.repo,
+							owner: repo.owner,
+							title: entryInfo.title,
+							body: entryInfo.content,
+						});
+					}
+				}
+			}
+		}
+
+		console.log("There were a total of " + newEntries["apcs-mykolyk"].length + " new entries for APCS.");
+		console.log("There were a total of " + newEntries["systems-dw"].length + " new entries for Systems.");
+		console.log("There were a total of " + newEntries["graphics-dw"].length + " new entries for Graphics.");
+		console.log("There were a total of " + newEntries["softdev-mykolyk"].length + " new entries for SoftDev.");
+		console.log("There were a total of " + newEntries["staging"].length + " new entries for Staging.");
+
+		console.log("There were a total of " + targetRepos.length + " new issues to create.");
+
+		// Now we'll create the issues.
+		for (let repo of targetRepos) {
+			app.createIssue(repo.owner, repo.repo, repo.title, repo.body);
+		}
+
+		console.log("Done!");
+	}, 60_000);
 };
--- src/index.js
+++ src/index.js
@@ -1,42 +1,20 @@
-const checkForNewEntries = require('./utils/checkForNewEntries');
-const parseEntryInformation = require('./utils/parseEntryInformation');
+const { Server, Probot } = require("probot");
+const app = require("./bot/index.js");
 const fs = require('fs');
-const path = require('path');
 
-const bot = require('./bot/index');
+const privateKey = fs.readFileSync(__dirname + '/data/private-key.pem', 'utf8')
+const config = require("./data/config.json");
 
-// Every minute, check for new entries in the feeds.
-setInterval(async () => {
-	// Scrape the feeds for new entries.
-	let targetRepos = [];
-	let newEntries = {};
-	newEntries["apcs-mykolyk"] = await checkForNewEntries("https://www.stuycs.org/apcs-mykolyk/feed.xml", "apcs-mykolyk.xml");
-	newEntries["systems-dw"] = await checkForNewEntries("https://www.stuycs.org/systems-dw/feed.xml", "systems-dw.xml");
-	newEntries["graphics-dw"] = await checkForNewEntries("https://www.stuycs.org/graphics-dw/feed.xml", "graphics-dw.xml");
-	newEntries["softdev-mykolyk"] = await checkForNewEntries("https://www.stuycs.org/softdev-mykolyk/feed.xml", "softdev-mykolyk.xml");
-	newEntries["staging"] = await checkForNewEntries("https://stuycs-bot-test.thundrredstar.repl.co/feed.xml", "staging.xml");
+async function startServer() {
+    const server = new Server({
+        Probot: Probot.defaults({
+            appId: config.appId,
+            privateKey: privateKey,
+            secret: config.secret,
+        }),
+    });
 
-	// Now we'll fetch the data for each class and create an issue for each new entry.
-	const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "data.json"), { encoding: "utf8" }));
-	for (let className in newEntries) {
-		if (newEntries[className].length > 0) {
-			const repos = data[className];
-			for (let entry of newEntries[className]) {
-				const entryInfo = parseEntryInformation(entry);
-				for (let repo of repos) {
-					targetRepos.push({
-						repo: repo.repo,
-						owner: repo.owner,
-						title: entryInfo.title,
-						body: entryInfo.content,
-					});
-				}
-			}
-		}
-	}
+    await server.load(app);
 
-	// Now we'll create the issues.
-	for (let repo of targetRepos) {
-
-	}
-});
+    server.start();
+}
