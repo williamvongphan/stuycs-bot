ceba0ee94b68fcd799dbf4ba7335185baad913ee
ThunderRedStar <wvongphanith30@stuy.edu>
2022-09-09 18:17:24 -0400

    Remove unnecessary file

--- test.js
+++
@@ -1,9 +0,0 @@
-const Axios = require('axios');
-const xml2js = require('xml2js');
-
-(async () => {
-	const xml = await Axios.get('https://www.stuycs.org/apcs-mykolyk/feed.xml');
-	let xmlString = xml.data;
-	let json = await xml2js.parseStringPromise(xmlString);
-	console.log(JSON.stringify(json, null, 2));
-})();
